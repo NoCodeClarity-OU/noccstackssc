5fb28f1ab497426ff7c8a572515214389741f2a4
alirazacodes <aliraza.ali954@gmail.com>
2024-12-13 06:33:58 -0800

    fix

--- src/noccstacksv2/crew.py
+++ src/noccstacksv2/crew.py
@@ -10,15 +10,16 @@ from .tools.custom_tool import ClarityDocScraper, SmartContractGenerator, TestGe
 class Noccstacksv2():
 	"""Noccstacksv2 crew for developing Clarity smart contracts"""
 
-	# Add default inputs to handle missing input cases
+	def __init__(self):
+		super().__init__()
+		self.inputs = self.default_inputs.copy()
+
+	# Default inputs that will be used if not provided
 	default_inputs = {
 		"project_name": "Newsletter",
 		"project_description": "A decentralized newsletter platform built on Stacks blockchain that allows users to subscribe, publish content, and manage subscriptions. Features include subscription management, content publishing, payment handling, and governance features."
 	}
 
-	# Learn more about YAML configuration files here:
-	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
@@ -53,42 +54,41 @@ class Noccstacksv2():
 	@task
 	def analyze_project(self) -> Task:
 		return Task(
-			config=self.tasks_config['analyze_project']
+			config=self.tasks_config['analyze_project'],
+			input=self.inputs
 		)
 
 	@task
 	def develop_smart_contract(self) -> Task:
 		return Task(
-			config=self.tasks_config['develop_smart_contract']
+			config=self.tasks_config['develop_smart_contract'],
+			input=self.inputs,
+			dependencies=[self.analyze_project]
 		)
 
 	@task
 	def create_tests(self) -> Task:
 		return Task(
-			config=self.tasks_config['create_tests']
+			config=self.tasks_config['create_tests'],
+			input=self.inputs,
+			dependencies=[self.analyze_project, self.develop_smart_contract]
 		)
 
 	@crew
 	def crew(self) -> Crew:
 		"""Creates the Noccstacksv2 crew for smart contract development"""
-		# To learn how to add knowledge sources to your crew, check out the documentation:
-		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
-		# Create crew with input handling
-		crew = Crew(
+		return Crew(
 			agents=self.agents,
 			tasks=self.tasks,
 			process=Process.sequential,
 			verbose=True,
+			inputs=self.inputs
 		)
-		
-		return crew
 
 	# Add before_kickoff decorator to handle input merging
 	@before_kickoff
 	def before_kickoff_function(self, inputs):
 		"""Merge default inputs with provided inputs"""
-		merged_inputs = self.default_inputs.copy()
 		if inputs:
-			merged_inputs.update(inputs)
-		return merged_inputs
+			self.inputs.update(inputs)
+		return self.inputs
